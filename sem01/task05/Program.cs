666d50622e520cb9341f75b95b8625056eac89b8
sergey <shkaev.ss@mail.ru>
2022-10-26 20:55:30 +0300

    created task06

--- sem01/task05/Program.cs
+++ sem01/task05/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления модуля числа.
+// 	2 -> 2
+// -3 -> 3
+// -7 -> 7
+Console.Write("Please type the number > ");
+int num = Convert.ToInt32(Console.ReadLine());
+int mod = num;
+if (mod < 0) mod = -num;
+Console.WriteLine("modul' chisla " + num + " = " + mod);
