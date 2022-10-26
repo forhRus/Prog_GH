fb5840f95d84a49e6394b637c56575678da7c3a5
sergey <shkaev.ss@mail.ru>
2022-10-26 18:39:01 +0300

    task03 +

--- sem01/task03/Program.cs
+++ sem01/task03/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число > ");
+string num = Console.ReadLine();
+int a = Convert.ToInt32(num);
+
+int f(int x)
+{
+    int cube = x * x * x;
+    return cube;
+}
+
+System.Console.WriteLine(f(a));
