9b3aa1e1c186482cbe62af2a265711d0fd93a6ed
sergey <shkaev.ss@mail.ru>
2022-10-26 20:01:10 +0300

    changed task02

--- sem01/task02/Program.cs
+++ sem01/task02/Program.cs
@@ -1,20 +1,12 @@
 ﻿System.Console.WriteLine("What is day today? 1-7 >");
-string day = Console.ReadLine();
-int num = Convert.ToInt32(day);
+int n = Convert.ToInt32(Console.ReadLine());
+string[] days = { "пн", "вт", "ср", "чт", "пт", "сб", "вс" };
 
-int mon = 1;
-int the = 2;
-int wen = 3;
-int thu = 4;
-int fry = 5;
-int sut = 6;
-int sun = 7;
-
-if (num == mon) System.Console.WriteLine("today is Monday");
-if (num == the) System.Console.WriteLine("today is Thesday");
-if (num == wen) System.Console.WriteLine("today is Wensday");
-if (num == thu) System.Console.WriteLine("today is Thusday");
-if (num == fry) System.Console.WriteLine("today is Fryday");
-if (num == sut) System.Console.WriteLine("today is Suterday");
-if (num == sun) System.Console.WriteLine("today is Sunday");     
-if (num <1 || num > 7) System.Console.WriteLine("eror!"); 
+if (n > 0 && n < 8) 
+{
+    Console.WriteLine(days[n-1]);
+}
+else
+{
+    Console.WriteLine("некоректные данные. введите от 1 до 7!");
+}
