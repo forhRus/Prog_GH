79feaa099ebf7e419e5fe6059aca89c7ee36740a
sergey <shkaev.ss@mail.ru>
2022-10-26 19:44:25 +0300

    task04 +

--- sem01/task04/Program.cs
+++ sem01/task04/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.Write("Введите число > ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+if (x < 0)
+{
+    int i = x;
+    int j = x * -1;
+    while (i < j)
+    {
+        System.Console.Write(i + ", " );
+        i++;
+    }
+    System.Console.WriteLine(j);
+}
+else
+{
+    if(x > 0)
+    {
+    int i = x * -1;
+    int j = x;
+    while (i < j)
+    {
+        System.Console.Write(i + ", " );
+        // System.Console.Write(", ");
+        i++;
+    }
+    System.Console.WriteLine(j);
+    }
+    else
+    {
+    System.Console.WriteLine(x);
+    }
+}
+
