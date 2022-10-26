b2336635779735328ea15c7efb3c6b45aa2769ab
sergey <shkaev.ss@mail.ru>
2022-10-26 22:01:00 +0300

    task06 +

--- sem01/task06/Program.cs
+++ sem01/task06/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+// 1 -> 1 
+// 2 -> 0.5
+// 0.25 -> 4
+System.Console.Write("Please type the number ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+double funV(double arg)
+{
+    double i = 1 / arg;
+    return i;
+} 
+;
+Console.WriteLine($"Обратное число от {num} является {funV(num):f2}.");
