ef45692b284d3f9a9ca66b9aac059283c3b508f2
sergey <shkaev.ss@mail.ru>
2022-10-26 16:44:32 +0300

    task01 +

--- sem01/task01/Program.cs
+++ sem01/task01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Give me num1 > ");
+string inNum1 = Console.ReadLine();
+int a = Convert.ToInt32(inNum1);
+
+System.Console.Write("Give me num2 > ");
+string inNum2 = Console.ReadLine();
+int b = Convert.ToInt32(inNum2);
+
+if (a * a == b)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
