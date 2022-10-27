6373ba85f2684ff143089dbd713565a7f96dc479
sergey <shkaev.ss@mail.ru>
2022-10-27 20:04:47 +0300

    task05 +

--- sem02/task05/Program.cs
+++ sem02/task05/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- 
+﻿// 2.1. Напишите программу, которая выводит случайное число из отрезка [10, 9999] и показывает наибольшую цифру числа.
+// 7812 -> 8 
+// 1213-> 3 
+// 845 -> 8
+
+int num = new Random().Next(10, 1000);
+Console.WriteLine(num);
+int maxDigit = num % 10;
+while(num > 0)
+{
+    int temp = num % 10;
+    if(maxDigit < temp) maxDigit = temp;
+    num = num / 10;
+}
+Console.WriteLine(maxDigit);
