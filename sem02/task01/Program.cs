b5d7e340214c34341ec1285d4f9c84e0c2c62371
sergey <shkaev.ss@mail.ru>
2022-10-27 16:50:00 +0300

    task01 +

--- sem02/task01/Program.cs
+++ sem02/task01/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает
+//  на вход трёхзначное число и на выходе 
+// показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100 || number >= 1000)
+    {
+        System.Console.WriteLine("Это число не трёхзначное.");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Ведите трёхзначное число: ");
+if(ValidateNumber(number))
+{
+    int lastDigit = number % 10;
+    System.Console.WriteLine($"последняя цифра {number} является {lastDigit}.");
+}
+
+
+
