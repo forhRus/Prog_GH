fc32f6c273ea81f873344f57c6c9ef4e80f72aaa
sergey <shkaev.ss@mail.ru>
2022-10-27 17:47:04 +0300

    task03 +

--- sem02/task03/Program.cs
+++ sem02/task03/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая будет принимать на вход два числа
+//  и выводить, является ли второе число кратным первому. Если число 2
+//  не кратно числу 1, то программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number; 
+}
+
+int num1 = Prompt("Введите первое число > ");
+int num2 = Prompt("Введите второе число > ");
+
+void crat(int a, int b)
+{
+    double ost = a % b;
+    if(ost == 0) Console.WriteLine($"число {a} кратно {b}.");
+    else Console.WriteLine($"число {a} не кратно {b}. Остаток {ost}.");
+}
+
+crat(num1, num2);
