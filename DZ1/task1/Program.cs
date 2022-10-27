c3d8daaa38418ab6283dbd78e253e5cf15b28a29
sergey <shkaev.ss@mail.ru>
2022-10-27 12:19:45 +0300

    dz-task1 +

--- DZ1/task1/Program.cs
+++ DZ1/task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+System.Console.Write("Введите первое число > ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число > ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2) Console.WriteLine($"{num1} > {num2}.");
+if (num1 < num2) Console.WriteLine($"{num2} > {num1}.");
+if (num1 == num2) Console.WriteLine($"{num1} = {num2}.");
