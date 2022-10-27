c6df77be5a0cf3253e244e7ad734f13d57dc1819
sergey <shkaev.ss@mail.ru>
2022-10-27 12:39:16 +0300

    dz-task3 +

--- DZ1/task3/Program.cs
+++ DZ1/task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+//           является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Введите число > ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0) 
+{
+    Console.WriteLine($"число {num} - чётное.");
+}
+else
+{
+    Console.WriteLine($"число {num} - нечётное.");
+}
