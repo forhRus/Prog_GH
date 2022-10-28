6b8d398c4480cd83f5aa2cba4f406c7af75a0782
sergey <shkaev.ss@mail.ru>
2022-10-28 14:44:52 +0300

    sem03/task02 +

--- Seminar/sem03/task02/Program.cs
+++ Seminar/sem03/task02/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool ValidateQuarter(int q)
+{
+    if (q > 0 && q < 5) return true;
+    else
+    {
+        Console.WriteLine("Введено не корректное значение.");
+        return false;
+    }
+}
+
+(int, int) FindQuarter(int q)
+{
+    if (q == 1) return (1, 1);
+    if (q == 2) return (-1, 1);
+    if (q == 3) return (-1, -1);
+    return (1, -1);
+}
+
+int quarter = Prompt("Введите четверть => ");
+
+if (ValidateQuarter(quarter))
+{
+    (int x, int y) = FindQuarter(quarter);
+    Console.WriteLine($"Для четверти {quarter} используйте шаблон {x}, {y}.");
+}
