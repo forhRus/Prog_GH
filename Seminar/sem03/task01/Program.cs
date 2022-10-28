0b708ef8a792da96c2b35d415a678e8ceae3ed37
sergey <shkaev.ss@mail.ru>
2022-10-28 12:47:43 +0300

    sem03/task01 +

--- Seminar/sem03/task01/Program.cs
+++ Seminar/sem03/task01/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());  
+}
+
+bool ValidateCoord(int x, int y)
+{
+    if(x == 0 || y == 0)
+    {
+        Console.WriteLine("Точка находиться на одной из координатных прямых");
+        return false;
+    }
+    return true;
+}
+
+int GetQuarter(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x > 0 && y < 0) return 4;
+    return 3;
+}
+
+int x = Prompt("Введите х => ");
+int y = Prompt("Введите y => ");
+
+if(ValidateCoord(x, y))
+{
+    int quarter = GetQuarter(x, y);
+    Console.WriteLine($"кородината находится в {quarter} четверти.");
+}
