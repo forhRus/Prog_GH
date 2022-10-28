ca221a0bef4ecc763afdfbd6958c7a1feedf985d
sergey <shkaev.ss@mail.ru>
2022-10-28 10:48:00 +0300

    task3 +

--- DZ/dz2/task3/Program.cs
+++ DZ/dz2/task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+int Prompt(string message) // функция на ввод числа
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число > "); //присваиваем значение через ввод числа
+
+// bool check(int num) //проверка числа.
+// {
+//     if (num < -99 || num > 99) return true;
+//     else return false;
+// }
+
+void res(int num)
+{
+    num = num / 100;
+    if (num == 0)
+    {
+        Console.WriteLine($"Некорректные данные. Введёное число не трёхзначное.");
+    }
+    else
+    {
+        int dig3 = num % 10;
+        Console.WriteLine($"третья цифра {dig3}.");
+    }
+}
+res(number);
