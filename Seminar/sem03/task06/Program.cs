57b2a1e8e3aae36ece916663408cce1c6efa3f01
sergey <shkaev.ss@mail.ru>
2022-10-29 19:48:11 +0300

    sem03/task06/ +

--- Seminar/sem03/task06/Program.cs
+++ Seminar/sem03/task06/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6*: Напишите программу, которая генерирует 
+// последовательность случайных чисел до тех пор, 
+// пока не сгенерирует кратное число введенному в начале.
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int crat = Prompt("Введите число от 1 до 10 > ");
+
+void Starter()
+{
+    while (true)
+    {
+        int num = new Random().Next(1, 11);
+        if (num % crat == 0)
+        {
+            System.Console.WriteLine(num);
+            break;
+        }
+        System.Console.WriteLine($"пока не получилось = {num}");
+    }
+}
+Starter();
+
+
+
