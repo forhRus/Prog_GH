d3e9c662e2b637229cf1cdbb8e7bc1f9109fea4f
sergey <shkaev.ss@mail.ru>
2022-10-30 13:23:05 +0300

    dz3/task3/ +

--- DZ/dz3/task3/Program.cs
+++ DZ/dz3/task3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string massege) //функция на ввод значения с консоли.
+{
+    System.Console.Write(massege);
+    return int.Parse(Console.ReadLine());
+}
+
+int number = Prompt("Введите число больше 0 > "); // задаём число с консоли.
+
+bool Validate(int number) //проверка на положительное число.
+{
+    if(number > 0) return true;
+    return false;
+}
+
+void Power(int number) // выводим последовательность до "number"
+{
+    if (Validate(number))
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            System.Console.Write($"{Math.Pow(i, 3)} "); //возводим в третью степень "i"
+        }
+        System.Console.WriteLine();  
+    }
+    else System.Console.WriteLine("Некорректные данные.");
+}
+
+Power(number);
