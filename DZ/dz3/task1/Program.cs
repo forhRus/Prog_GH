4e493e2933b244af85befac838a5dc5b4451b684
sergey <shkaev.ss@mail.ru>
2022-10-30 08:15:06 +0300

    dz3/task1 changed  & dz3/task2 +

--- DZ/dz3/task1/Program.cs
+++ DZ/dz3/task1/Program.cs
@@ -1,39 +1,56 @@
 ﻿// Задача 1: Напишите программу, которая принимает на 
 // вход пятизначное число и проверяет, является ли оно палиндромом.
 
-int Prompt(string msg) // функция на ввод числа с консоли
+int Prompt(string massege) // функция на ввод числа с консоли
 {
-    System.Console.WriteLine(msg);
+    System.Console.WriteLine(massege);
     return int.Parse(Console.ReadLine());
 }
 
 int number = Prompt("Введите пятизначное число > "); //задаём число
 
-bool Validate(int num) // функция для проверки на пятизначность
+int Counter(int num) //счётчик цифр в числе
 {
-    if(num > 9999 && num < 100000) return true;
-    else 
+    int count = 0;
+    while (num > 0 || num < 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+bool Validate(int num) // функция для проверки на пятизначность. позволяет работать с отрицательными числами.
+{
+    int count = Counter(num);
+    if(count == 5) return true;
+    else
     {
         System.Console.WriteLine($"Число {num} не пятизначное.");
         return false;
     }
 }
 
-void CheckPol(int num) //проверяем число. палиндром иль нет
+int Pal(int num) //функция на разворот числа / хотел через массив решить, но не справился.
 {
-    if(Validate(num)) // проверка на пятизначность
+    int pal = 0; // будущий "палиндром"
+    int x = num;
+    for (int i = 0; i < 5; i++) //разворачиваем число
     {
-        int pol = 0; // будущий "палиндром"
-        int x = num;
-        for (int i = 0; i < 5; i++) //разворачиваем число
-        {
-            pol = pol + x % 10;
-            if(i < 4) pol = pol * 10;
-            x = x / 10;
-        }
-        if(pol == num) System.Console.WriteLine($"число {number} - палиндром. {pol}"); // вывод палиндром
-        else System.Console.WriteLine($"число {number} - непалиндром. {pol}"); //вывод непалиндром
+        pal = pal + x % 10; // каждый раз добавляем остаток
+        if (i < 4) pal = pal * 10; //чтобы не получился палиндом с нулём на конце, 5ую цифру только прибавляем
+        x = x / 10; //уменьшаем число
+    }
+    return pal;
+}
+
+void CheckPol(int num) //сравниваем число и его "палиндром"
+{
+    if (Validate(num)) //проверка
+    {
+        if (Pal(num) == num) System.Console.WriteLine($"число {number} - палиндром. Для наглядности {Pal(num)}"); // вывод палиндром
+        else System.Console.WriteLine($"число {number} - непалиндром. Для наглядности {Pal(num)}"); //вывод непалиндром
     }
 }
 
-CheckPol(number); //зовём проверку заданного числа
+CheckPol(number); //зовём проверку на палиндромность заданного числа
--- DZ/dz3/task2/Program.cs
+++ DZ/dz3/task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+(int x, int y, int z) GetCoord(string massege) // функция для ввода кординат х, у, z какой-то "massege" точки
+{
+    System.Console.Write($"Введите координату х для точки {massege} > ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write($"Введите координату y для точки {massege} > ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write($"Введите координату z для точки {massege} > ");
+    int z = Convert.ToInt32(Console.ReadLine());
+    return (x, y, z);
+}
+// задали точки.
+(int xa, int ya, int za) = GetCoord("a");
+(int xb, int yb, int zb) = GetCoord("b");
+
+double Length(int x1, int y1, int z1, int x2, int y2, int z2) //функция считающая растояние между точками в трёхмерном пространстве. 
+{
+    int x = x1 - x2;
+    int y = y1 - y2;
+    int z = z1 - z2;
+    double length = Math.Sqrt((x * x) + (y * y) + (z * z));
+    return length;
+}
+System.Console.WriteLine($"Расстояние между точками {Length(xa, ya, za, xb, yb, zb):f2}.");
