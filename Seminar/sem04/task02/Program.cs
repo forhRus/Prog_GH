819838c3bff7bb86e989a6db8024ef2bdb9606f7
sergey <shkaev.ss@mail.ru>
2022-11-08 18:51:44 +0300

    sem04/task02/ +

--- Seminar/sem04/task02/Program.cs
+++ Seminar/sem04/task02/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int number = Prompt("Задайте число, и мы посчитаем сколько в нём цифр. -> ");
+
+int Counter(int number)
+{
+    int count = 0;
+    int num = number;
+    while (num > 0 || num < 0)
+    {
+        num /= 10;
+        count++;
+        // if(num == 0) break;
+    }
+    return count;
+}
+
+System.Console.WriteLine($"в числе {number} - {Counter(number)} цифр.");
