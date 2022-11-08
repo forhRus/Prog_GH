bb4ea2ee95660594970cceb8d5c015f60fe1a175
sergey <shkaev.ss@mail.ru>
2022-11-08 17:04:49 +0300

    dz3/task1_1/ +

---
+++ DZ/dz3/task1_1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 1: Напишите программу, которая принимает на 
+// вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int Prompt(string massege) // функция на ввод числа с консоли
+{
+    System.Console.Write(massege);
+    return int.Parse(Console.ReadLine());
+}
+
+int number = Prompt("Введите пятизначное число > "); //задаём число
+
+int Revers(int number)
+{
+    int answer = 0;
+    while(number > 0)
+    {
+        answer = answer *10 + number%10;
+        number /=10;
+    }
+    return answer;
+}
+
+int Counter(int num) //счётчик цифр в числе
+{
+    int count = 0;
+    while (num > 0 || num < 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+bool Validate(int num) // функция для проверки на пятизначность. позволяет работать с отрицательными числами.
+{
+    int count = Counter(num);
+    if(count == 5) return true;
+    else
+    {
+        System.Console.WriteLine($"Число {num} не пятизначное.");
+        return false;
+    }
+}
+
+void CheckPal(int num) //сравниваем число и его "палиндром"
+{
+    if (Validate(num)) //проверка
+    {
+        if (Revers(num) == num) System.Console.WriteLine($"число {number} - палиндром. Для наглядности {Revers(num)}"); // вывод палиндром
+        else System.Console.WriteLine($"число {number} - непалиндром. Для наглядности {Revers(num)}"); //вывод не палиндром
+    }
+}
+
+CheckPal(number); //зовём проверку числа - палиндром ли оно?
