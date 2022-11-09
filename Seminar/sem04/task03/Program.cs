7a21f4e84a0d842bb11d844260493fbd26712822
sergey <shkaev.ss@mail.ru>
2022-11-09 13:59:48 +0300

    sem04/task04/ +

---
+++ Seminar/sem04/task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    return int.Parse(Console.ReadLine());
+}
+
+int number = Prompt("Введите число -> ");
+
+int Fact(int number)
+{
+    int fact = 1;
+    for( int i = 1; i <= number; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+System.Console.WriteLine($"произведение всех цифр в числе {number} = {Fact(number)}");
---
+++ Seminar/sem04/task04/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] CreatArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write($", {array[i]}");
+    }
+    System.Console.WriteLine("");
+}
+
+
+System.Console.Write($"Массив из 8 элементов из 0 и 1 -> ");
+PrintArray(CreatArray(8));
